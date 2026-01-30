66cc057d0b277558b4fb78367ccc597d02771929
6804860016-prog <6804860016@siam.edu>
2026-01-30 11:06:34 +0700

    fix UserController.java

--- src/main/java/com/example/LoginApp/UserController.java
+++ src/main/java/com/example/LoginApp/UserController.java
@@ -56,7 +56,7 @@ public class UserController {
             res.put("redirect", "/classA.html");
         } else if ("B".equalsIgnoreCase(cls)) {
             res.put("redirect", "/classB.html");
-        } else if ("GEDI".equalIgnoreCase(cls)) {
+        } else if ("GEDI".equalsIgnoreCase(cls)) {
             res.put("redirect", "/GEDinstructors.html");
         } else {
             res.put("redirect", "/unassigned.html");
